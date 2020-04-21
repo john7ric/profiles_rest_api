aec4cb9a48e1225a65d6aed183efe6f9382ce9cb
A-7222 <richard.j.ext@flydubai.com>
2020-04-21 13:16:26 +0530

    post wip

---
+++ profiles_api/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+
+class UserSearializer(serializers.Serializer):
+    """ serializer for user class """
+    
+    
+    name = serializers.CharField(max_length = 10)
+    age = serializers.IntegerField(max_value = 99, min_value = 1)
--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,10 +1,16 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+from profiles_api import serializers
 
 # Create your views here.
 
 class UserAPIView(APIView):
     """ APIView for managing user profiles """
+    
+    serializer_class = serializers.UserSearializer
+    
+    
     def get(self, request, format=None):
         """ method for handling get request to this class """
 
@@ -14,3 +20,23 @@ class UserAPIView(APIView):
         ':D:D:D',
         ]
         return Response({'about' : 'Hello there ','content' : an_apiview, 'reaction' : 'finally full stack'})
+        
+    def post(self, request):
+        """ method handling the post requests to this view """
+        
+        serializer = sel.serializer_class(request.data)
+        
+        if serializer.is_valid():
+            #message_string = f'Hello {serializer.validated_data.get('name')} you are {serializer.validated_data.get('age')} years old'
+            
+            name = serializer.validated_data.get('name')
+            age = serializer.validated_data.get('age')
+            message_string = 'Hello ' + name + " you are " + age + " old"
+            return Response({'message' : message_string})
+        else:
+            return Response({'errors' : serializer.errors, 'status' : status.HTTP_400_BAD_REQUEST })
+                
+        
+                           
+    
+        
